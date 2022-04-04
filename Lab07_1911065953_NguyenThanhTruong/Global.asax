adbc0b2699b8a9ede09bc6d536d434e9ca407393
thanhtruong0704@gmail.com <thanhtruong0704@gmail.com>
2022-04-04 14:40:02 +0700

    Add project files.

---
+++ Lab07_1911065953_NguyenThanhTruong/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab07_1911065953_NguyenThanhTruong.WebApiApplication" Language="C#" %>
---
+++ Lab07_1911065953_NguyenThanhTruong/Models/Sach.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab07_1911065953_NguyenThanhTruong.Models
+{
+    public class Sach
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string AuthorName { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
